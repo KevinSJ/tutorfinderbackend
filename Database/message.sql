04a633debfc10cfdbfdd642595cece65d31228ac
Kevin <sjiang42@wisc.edu>
2016-04-08 19:08:42 -0500

    SQL update

---
+++ Database/message.sql
@@ -0,0 +1,17 @@
+﻿-- Table: public.message
+
+-- DROP TABLE public.message;
+
+CREATE TABLE public.message
+(
+  sender character varying(1024) NOT NULL,
+  receiver character varying(1024) NOT NULL,
+  message character varying(1024) NOT NULL,
+  id integer NOT NULL DEFAULT nextval('message_id_seq'::regclass),
+  CONSTRAINT message_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.message
+  OWNER TO postgres;
---
+++ Database/student.sql
@@ -0,0 +1,32 @@
+﻿-- Table: public.student
+
+-- DROP TABLE public.student;
+
+CREATE TABLE public.student
+(
+  degree character varying(1024) NOT NULL,
+  description character varying(1024),
+  location point NOT NULL,
+  gender character varying(1024) NOT NULL,
+  tutor boolean NOT NULL,
+  rating integer NOT NULL,
+  name character varying(1024),
+  realm character varying(1024),
+  username character varying(1024),
+  password character varying(1024) NOT NULL,
+  credentials character varying(1024),
+  challenges character varying(1024),
+  email character varying(1024) NOT NULL,
+  emailverified boolean,
+  verificationtoken character varying(1024),
+  status character varying(1024),
+  created timestamp with time zone,
+  lastupdated timestamp with time zone,
+  id integer NOT NULL DEFAULT nextval('student_id_seq'::regclass),
+  CONSTRAINT student_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.student
+  OWNER TO postgres;
