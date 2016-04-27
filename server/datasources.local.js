047d93a2e8b58c6ddd7867dacfac392cb0440c64
Kevin S Jiang <sjiang42@wisc.edu>
2016-04-27 17:31:16 -0500

    should work

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -2,9 +2,7 @@ var pgUri = process.env.DATABASE_URL||localhost
 
 module.exports = {
     "TutorFinderdb": {
-    "host": pgUri,
-    "port": 5432,
-    "database": "d2c6koeekdivfk",
+    "url":pgUri   
     "password": "Flf1oYUfEOgoWauu2S9ndRGi0Y",
     "name": "TutorFinderdb",
     "user": "uodhujnipeltjp",
