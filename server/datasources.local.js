e5c69c9a4d1d571b7821bc7518ef3f3541c27f87
Kevin S Jiang <sjiang42@wisc.edu>
2016-04-27 17:24:41 -0500

    update database

---
+++ server/datasources.local.js
@@ -0,0 +1,13 @@
+var pgUri = process.env.DATABASE_URL||localhost 
+
+module.exports = {
+    "TutorFinderdb": {
+    "host": "pgUri",
+    "port": 5432,
+    "database": "d2c6koeekdivfk",
+    "password": "Flf1oYUfEOgoWauu2S9ndRGi0Y",
+    "name": "TutorFinderdb",
+    "user": "uodhujnipeltjp",
+    "connector": "postgresql"
+    }
+}
