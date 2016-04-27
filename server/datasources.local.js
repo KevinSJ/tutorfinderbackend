d1adc349b9193e0b630adbf70ba7df3ead6b2add
Kevin S Jiang <sjiang42@wisc.edu>
2016-04-27 17:27:44 -0500

    update datasource

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -2,7 +2,7 @@ var pgUri = process.env.DATABASE_URL||localhost
 
 module.exports = {
     "TutorFinderdb": {
-    "host": "pgUri",
+    "host": pgUri,
     "port": 5432,
     "database": "d2c6koeekdivfk",
     "password": "Flf1oYUfEOgoWauu2S9ndRGi0Y",
